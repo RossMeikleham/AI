fd9e47f1d7000638e6e3b6c8d66b45f8ed0380c1
RossMeikleham <rossmeikleham@hotmail.co.uk>
2015-10-10 18:18:49 +0100

    Added ability to plot multiple graphs

--- lab1/Lab1.hs
+++ lab1/Lab1.hs
@@ -1,5 +1,7 @@
+
+import Data.Maybe
 import System.IO
-import Graphics.EasyPlot as P
+import Graphics.EasyPlot 
 
 -- | Sampling time for all samples in milliseconds
 samplingTime :: Int
@@ -28,27 +30,48 @@ idealOperatorDelay samples ms =
           offset = (length samples) `div` offsetRate  --Number of samples to "delay"
 
 
-normaliseMultiplier :: [Int] -> Double
-normaliseMultiplier xs = 1.0 / (fromIntegral $ maximum xs)
+-- Apply moving average with k1, k2 to a given list of signals
+--movingAverage :: [Int] -> Int -> Int -> Maybe [Int]
+--movingAverage signals k1 k2 = 
+
 
 -- Plot samples
-plotSamples :: [Int] -> [[Int]] -> IO ()
+plotSamples :: [Int] -> [(String, [Int])] -> IO ()
 plotSamples xs ys = do
-    plot X11 $ Data2D [Title "Sample Data", Style Lines] [] $ points $ normalise xs
+    plot X11 $ [Data2D [Title "Sample Data", Style Lines] [] $ points $ normalise xs]
+              ++ others
     return ()
 
     --lotList [] (points (normalise xs))
     where
+        others = map (\(g,col) -> Data2D [Title (fst g), Style Lines, Color col] [] 
+                            $ points $ normalise $ snd g) (zip ys (map getColor [0..]))
+
+        -- Normalise a set of samples between 1.0 and -1.0
         normalise :: [Int] -> [Double] 
         normalise = map (\y -> (fromIntegral y) / (fromIntegral $ maximum xs))
 
+        -- Generate (x,y) points from a list of samples
         points :: [Double] -> [(Double, Double)] 
-        points samples = zip  
+        points samples = zip
+                    -- x coodinates  
                     (map (\x -> fromIntegral x * 
                                     (fromIntegral samplingTime / 
                                     (fromIntegral $ length samples))
                          )  [0..]) 
-                     samples -- y co-ordinates
+                     -- y coordinates
+                     samples
+
+        -- Select line color from a selection of 7 colors
+        getColor :: Int -> Color
+        getColor n = case n `mod` 7 of
+            0 -> Red
+            1 -> Green
+            2 -> Black
+            3 -> Orange
+            4 -> Yellow
+            5 -> DarkOrange
+            _ -> Cyan
 
 labFilePath :: String
 labFilePath = "laboratory.dat"
@@ -63,6 +86,12 @@ main = do
     let samples = map (\s -> (read s :: Int)) strSamples 
     -- Calculate sampling rate 
     putStr $ "Sampling rate is: " ++ (show  $ samplingRate samples) ++ "Hz\n"
-    -- Plot our samples
-    plotSamples samples [] 
+    
+    -- Calc ideal operator delays for 5, 10, and 15 ms, and plot against
+    -- samples
+    let iods = map (idealOperatorDelay samples) [5, 10, 15]
+    plotSamples samples $ zip ["5ms delay", "10ms delay", "15ms delay"] (catMaybes iods)
+     
     hClose handle      
+
+
